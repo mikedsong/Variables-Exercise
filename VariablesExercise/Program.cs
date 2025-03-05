dc2ad6e6394a94fdbf76c61029c8866feb70e3d0
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-03-05 13:22:53 -0800

    Completed the exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Mike";
+            int age = 29;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial = 'M';
+            bool isMarried = false;
+            
+            Console.WriteLine($"Name : {myName}\nAge: {age}\nAverage {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
+            
+
         }
     }
 }
